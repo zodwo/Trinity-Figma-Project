a2b90ff93e34c2506c87892abc6f349d8ce22fbd
zodwo <senanm260@gmail.com>
2023-04-29 19:38:40 +0400

    Trinity

--- js/main2.js
+++ js/main2.js
@@ -1,22 +1,41 @@
+// OnClick obhect
+const hamburger = document.querySelector(".menu-logo"); // 1
+const lang = document.querySelector(".lang"); // 2
+const navigation = document.querySelector(".navigation"); // 3
+const popup = document.querySelector(".open-pop"); //4
 
+// be active elements
+const navBar = document.querySelector(".nav-col1"); // 1
+const langBar = document.querySelector(".nav-col3-txt-wrapper"); // 2
+const BarNav = document.querySelector(".navigator-dubai"); // 3
+const popupOp = document.querySelector(".pop-up-wrapper"); //4
+const menu_wrapper = document.querySelector(".list-main-wrapper"); //5
 
+//active
 
-hamburger = document.querySelector(".menu-logo");
-hamburger.onclick = function () {
-    navBar = document.querySelector(".nav-col1");
-    navBar.classList.toggle("active");
-}
+// Mobile Menu
+hamburger.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  menu_wrapper.classList.toggle("active");
+});
+// Mobile menu wrapper
+menu_wrapper.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  menu_wrapper.classList.toggle("active");
+});
 
+// Lang Menu
+lang.addEventListener("click", () => {
+  langBar.classList.toggle("active");
+});
 
-lang = document.querySelector(".lang");
-lang.onclick = function () {
-    langBar = document.querySelector(".nav-col3-txt-wrapper");
-    langBar.classList.toggle("active");
-}
+// right nav
+navigation.addEventListener("click", () => {
+  BarNav.classList.toggle("active");
+});
 
-navigation = document.querySelector(".navigation");
-navigation.onclick = function () {
-    BarNav = document.querySelector(".navigator-dubai");
-    BarNav.classList.toggle("active")
-}
+// pop up
 
+popup.addEventListener("click", () => {
+  popupOp.classList.toggle("active");
+});
